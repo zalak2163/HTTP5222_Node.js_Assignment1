cef08fd82bfcbad496ea40d0b5384639a4d929b6
zalak patel <znpatel2003@gmail.com>
2024-12-12 19:59:48 -0500

    change some code

--- netlify-functions/admin.js
+++
@@ -1,7 +0,0 @@
-// netlify-functions/admin.js
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Admin endpoint reached" }),
-  };
-};
